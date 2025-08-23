50c56324dd957117769962ddce5b4c9c71793e33
Trias <triasbrata@gmail.com>
2025-08-24 03:46:09 +0700

    add ready and release

--- pkgs/messagebroker/manager/impl/manCon.go
+++ pkgs/messagebroker/manager/impl/manCon.go
@@ -7,25 +7,45 @@ import (
 	"github.com/triasbrata/adios/pkgs/messagebroker/manager"
 )
 
-type manCon[T any] struct {
-	mutex *sync.RWMutex
-	con   *T
+type ShouldConnectionHave interface {
+	Close() error
+}
+type manCon[T ShouldConnectionHave] struct {
+	mutex     *sync.RWMutex
+	con       T
+	readyChan chan struct{}
 }
 
-// GetConAMQP implements manager.Manager.
-func (m *manCon[T]) GetCon() *T {
+// GetCon implements manager.Manager.
+func (m *manCon[T]) GetCon() T {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 	return m.con
 }
 
-// SetConAMQP implements manager.Manager.
-func (m *manCon[T]) SetCon(con *T) {
+// SetCon implements manager.Manager.
+func (m *manCon[T]) SetCon(con T) {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	m.con = con
+	go func() {
+		m.readyChan <- struct{}{}
+	}()
 }
 
-func NewManager() manager.Manager[amqp091.Connection] {
-	return &manCon[amqp091.Connection]{mutex: &sync.RWMutex{}}
+// Ready implements manager.Manager.
+func (m *manCon[T]) Ready() <-chan struct{} {
+	return m.readyChan
+}
+
+// SetConAMQP implements manager.Manager.
+func (m *manCon[T]) Release() error {
+	return m.con.Close()
+}
+
+func NewManager() manager.Manager[*amqp091.Connection] {
+	return &manCon[*amqp091.Connection]{
+		mutex:     &sync.RWMutex{},
+		readyChan: make(chan struct{}, 1),
+	}
 }
--- pkgs/messagebroker/manager/manager.go
+++ pkgs/messagebroker/manager/manager.go
@@ -1,8 +1,8 @@
 package manager
 
 type Manager[T any] interface {
-	SetCon(con *T)
-	GetCon() *T
-	Ready() chan struct{}
+	SetCon(con T)
+	GetCon() T
+	Ready() <-chan struct{}
 	Release() error
 }
