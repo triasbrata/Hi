12d449718b66d59db515d78a5596a0dc468ce61a
Trias <triasbrata@gmail.com>
2025-08-24 03:49:06 +0700

    create module message broker

---
+++ pkgs/messagebroker/fx.go
@@ -0,0 +1,29 @@
+package messagebroker
+
+import (
+	"github.com/triasbrata/adios/internals/config"
+	"github.com/triasbrata/adios/pkgs/messagebroker/broker"
+	"github.com/triasbrata/adios/pkgs/messagebroker/broker/impl"
+	"go.uber.org/fx"
+)
+
+func MessageBrokerAmqp(cfg ...impl.AmqpConfig) fx.Option {
+	provider := []fx.Option{}
+	if len(cfg) == 1 {
+		provider = append(provider, fx.Supply(cfg[0], fx.Private))
+	}
+	if len(cfg) == 0 {
+		provider = append(provider, fx.Provide(func(c *config.Config) impl.AmqpConfig {
+			return impl.AmqpConfig{
+				ConnectionName: c.Consumer.Amqp.ConnectionName,
+				URI:            c.Consumer.Amqp.URI,
+			}
+		}, fx.Private))
+	}
+	provider = append(provider, fx.Provide(func(cfg impl.AmqpConfig) (broker.Broker, error) {
+		return impl.CreateNewBroker(impl.WithAmqpBroker(cfg))
+
+	}))
+	return fx.Module("pks/messagebroker", provider...)
+
+}
